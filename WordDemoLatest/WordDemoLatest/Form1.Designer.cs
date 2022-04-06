0702090b745a2a0d01ba574caa8c4e92e818acdc
AkshayMorey <52403425+AkshayMorey@users.noreply.github.com>
2022-04-07 00:11:31 +0530

    Add files via upload

---
+++ WordDemoLatest/WordDemoLatest/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WordDemoLatest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mb = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(429, 73);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(78, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "C# To Create Word";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Type Message:";
+            // 
+            // mb
+            // 
+            this.mb.Location = new System.Drawing.Point(18, 108);
+            this.mb.Multiline = true;
+            this.mb.Name = "mb";
+            this.mb.Size = new System.Drawing.Size(399, 112);
+            this.mb.TabIndex = 2;
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.Gold;
+            this.b1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.ForeColor = System.Drawing.Color.Navy;
+            this.b1.Location = new System.Drawing.Point(18, 226);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(176, 46);
+            this.b1.TabIndex = 3;
+            this.b1.Text = "Create DocX";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(249, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.clear);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(429, 489);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.mb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word App Creation";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox mb;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WordDemoLatest/WordDemoLatest/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using Xceed.Words.NET;
+using System.Diagnostics;
+namespace WordDemoLatest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // First Button Now We can start coding part
+        // set the file path to create which can be local path or global path
+        // here i ve set global path instead of local path, because this app will be run on all machine (target all machine instead of one)
+        // this is optional one.
+        // this global path is done by using the Special Class Environment
+        string gpath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"/Akshay.docx";
+        private void b1_Click(object sender, EventArgs e)
+        {
+    // check the validation
+    // if text box is not empty then write code for docx generation else provide validation via Message Box alert message
+            if(mb.Text!="")
+            {
+                // get the user message dynmaically via Text Property of Text Box
+                string userMessage = mb.Text;
+                // create a new object for DocX class for the preparation of word document
+                DocX obj=DocX.Create(gpath);
+                // add paragraphs to new document via DocX obj
+                obj.InsertParagraph(userMessage);
+                // finally save the document using Save() or SaveAs() method
+                obj.Save();
+                // that's all. next step is to start / launch the newly created word document automatically via C#
+                // for that just use Static method of Process class Start()
+                // where first argument is the external application name to launch. here our application is word.
+                // so that, winword.exe is used
+                // second argument is the exact location of newly created word document which is saved in the variable gpath
+                Process.Start("winword.exe", gpath);
+                // that's all. Now we can run our application
+            }
+            else
+            {
+        // create an alert box using MessageBox class
+        // first argument is message and second argument is heading of the message box
+                MessageBox.Show("Text Box is Empty\nPlease type your Message...", "Empty");
+            }
+        }
+// Second Button
+        private void clear(object sender, EventArgs e)
+        {
+            // clear the text box
+            mb.Text = "";
+        }
+    }
+}
